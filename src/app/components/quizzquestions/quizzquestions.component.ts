f73d5f09a28b29fec66c54733a8c61940e437b3e
AnasMouak <anasmouak1111@gmail.com>
2024-10-13 20:49:25 +0100

    Refactor quizzquestions component to add radio buttons for answer choices

--- src/app/components/quizzquestions/quizzquestions.component.ts
+++ src/app/components/quizzquestions/quizzquestions.component.ts
@@ -2,19 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { QuizzService } from '../../services/quizz.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-quizzquestions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './quizzquestions.component.html',
-  styleUrl: './quizzquestions.component.css'
+  styleUrls: ['./quizzquestions.component.css']
 })
 export class QuizzquestionsComponent implements OnInit {
 
   questions: any[] = [];
   quizzId: number | undefined;
-  quizTitle: string | undefined;
+  selectedAnswers: string[] = [];
 
   constructor(private readonly quizzService: QuizzService, private readonly route: ActivatedRoute) { }
 
@@ -26,7 +27,6 @@ export class QuizzquestionsComponent implements OnInit {
       this.quizzService.getQuizQuestions(this.quizzId).subscribe({
         next: (data: any) => {
           this.questions = data;
-          this.quizTitle = data.title;
         },
         error: (err: any) => {
           console.error(err);
